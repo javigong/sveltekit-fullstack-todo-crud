a5ee056c41c1421d6592f800b46646ff0770721f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-12-14 11:13:08 -0800

    Add main layout and page route

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<div class="mainContainer">
+  <slot/>
+</div>
+
+<style>
+.mainContainer {
+  min-height: 100vh;
+  background: linear-gradient(to right, #021a31, #0808af);
+  color: white;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+</style>
