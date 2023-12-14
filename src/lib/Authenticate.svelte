bdc02557cba0d94ddf70d5d696793b451ddb428a
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-12-14 12:35:44 -0800

    Add register login form

--- src/lib/Authenticate.svelte
+++ src/lib/Authenticate.svelte
@@ -1,16 +1,62 @@
+<script>
+  let email = ''
+  let password = ''
+  let confirmPass = ''
+  let error = false
+  let register = false
+
+  function handleAuthenticate() {
+    if (!email || !password || (register && !confirmPass)) {
+      error = true
+      return
+    }
+  }
+
+  function handleRegister() {
+    register = !register
+  }
+</script>
+
 <div class="authContainer">
   <form action="">
-    <h1>Login</h1>
+    <h1>{register ? 'Register' : 'Login'}</h1>
+    {#if error}
+      <p class="error">The information you have entered is not correct</p>
+    {/if}
     <label>
-      <input type="email" placeholder="email" />
+      <p class={email ? ' above' : ' center'}>Email</p>
+      <input bind:value={email} type="email" placeholder="Email" />
     </label>
     <label>
-      <input type="email" placeholder="email" />
+      <p class={password ? ' above' : ' center'}>Password</p>
+      <input bind:value={password} type="password" placeholder="Password" />
     </label>
-    <label>
-      <input type="email" placeholder="email" />
-    </label>
+    {#if register}
+      <label>
+        <p class={confirmPass ? ' above' : ' center'}>Confirm Password</p>
+        <input
+          bind:value={confirmPass}
+          type="password"
+          placeholder="Confirm Password"
+        />
+      </label>
+    {/if}
+    <button type="button">Submit</button>
   </form>
+  <div class="options">
+    <p>Or</p>
+    {#if register}
+      <div>
+        <p>Already have an account?</p>
+        <button on:click={handleRegister}>Login</button>
+      </div>
+    {:else}
+      <div>
+        <p>Don't have and account?</p>
+        <button on:click={handleRegister}>Register</button>
+      </div>
+    {/if}
+  </div>
 </div>
 
 <style>
@@ -26,13 +72,136 @@
   form {
     display: flex;
     flex-direction: column;
-    gap: 8px;
+    gap: 14px;
+  }
+
+  form,
+  .options {
     width: 400px;
     max-width: 100%;
     margin: 0 auto;
   }
 
+  h1 {
+    text-align: center;
+    font-size: 3rem;
+  }
+
+  form label {
+    position: relative;
+    border: 1px solid navy;
+    border-radius: 5px;
+  }
+
   form input {
     width: 100%;
+    border: none;
+    background: transparent;
+    color: white;
+    padding: 14px;
+  }
+
+  form input:focus {
+    border: none;
+    outline: none;
+  }
+
+  form label:focus-within {
+    border-color: blue;
+  }
+
+  form button {
+    background: navy;
+    color: white;
+    border: none;
+    padding: 14px 0;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1rem;
+  }
+
+  form button:hover {
+    background: blue;
+  }
+
+  .above,
+  .center {
+    position: absolute;
+    transform: translateY(-50%);
+    pointer-events: none;
+    color: white;
+    border-radius: 4px;
+    padding: 0 6px;
+    font-size: 0.8rem;
+  }
+
+  .above {
+    top: 0;
+    left: 24px;
+    background: navy;
+    border: 1ps solid blue;
+    font-size: 0.7rem;
+  }
+
+  .center {
+    top: 50%;
+    left: 6px;
+    border: 1px solid transparent;
+    opacity: 0;
+  }
+
+  .error {
+    color: coral;
+    font-size: 0.9rem;
+  }
+
+  .options {
+    padding: 14px 0;
+    overflow: hidden;
+    font-size: 0.9rem;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+  }
+
+  .options > p {
+    position: relative;
+    text-align: center;
+    width: fit-content;
+    margin: 0 auto;
+    padding: 0 8px;
+  }
+
+  .options > p::after,
+  .options > p::before {
+    position: absolute;
+    content: '';
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100vw;
+    height: 1.5px;
+    background-color: white;
+  }
+
+  .options > p:before {
+    left: 100%;
+  }
+  .options > p:after {
+    right: 100%;
+  }
+
+  .options div {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    justify-content: center;
+  }
+
+  .options div button {
+    color: cyan;
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+    font-size: 1rem;
   }
 </style>
