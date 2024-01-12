30662fbff8213719e8e6d1d32820fa60482616ba
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-01-12 00:11:02 -0800

    Add store and db data updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,70 @@
+<script>
+  import { auth, db } from '$lib/firebase/firebase'
+  import { doc, getDoc, setDoc } from 'firebase/firestore'
+  import { onMount } from 'svelte'
+  import { authStore } from '../store/store'
+
+  const nonAuthRoutes = ['/', 'aboutus']
+  onMount(() => {
+    console.log('Mounting')
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      const currentPath = window.location.pathname
+
+      if (!user && !nonAuthRoutes.includes(currentPath)) {
+        window.location.href = '/'
+        return
+      }
+
+      if (user && currentPath === '/') {
+        window.location.href = '/dashboard'
+        return
+      }
+
+      /**
+       * @type {import("@firebase/firestore").DocumentData}
+       */
+      let dataToSetToStore
+      const docRef = user ? doc(db, 'users', user.uid) : null
+      const docSnap = docRef ? await getDoc(docRef) : null
+
+      if (user && !docSnap?.exists()) {
+        const userRef = doc(db, 'user', user.uid)
+        dataToSetToStore = {
+          email: user.email,
+          todos: [],
+        }
+        await setDoc(userRef, dataToSetToStore, { merge: true })
+      }
+
+      if (user && docSnap?.exists()) {
+        const userData = docSnap.data()
+        dataToSetToStore = userData
+      }
+
+      if (user) {
+        authStore.update((curr) => ({
+          ...curr,
+          // @ts-ignore
+          user,
+          data: dataToSetToStore,
+          loading: false,
+        }))
+      }
+    })
+  })
+</script>
+
 <div class="mainContainer">
-  <slot/>
+  <slot />
 </div>
 
 <style>
-.mainContainer {
-  min-height: 100vh;
-  background: linear-gradient(to right, #062543, #010126);
-  color: white;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-}
+  .mainContainer {
+    min-height: 100vh;
+    background: linear-gradient(to right, #062543, #010126);
+    color: white;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
 </style>
